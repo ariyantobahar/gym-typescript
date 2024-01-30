98ae428a8ec735b53fd91e9820cdc294bbef563c
ariyantobahar <33273280+ariyantobahar@users.noreply.github.com>
2024-01-30 15:31:58 +0700

    fix error

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ function App(){
     <Benefits setSelectedPage={setSelectedPage}/>
     <OurClasses setSelectedPage={setSelectedPage}/>
     <ContactUs setSelectedPage={setSelectedPage}/>
-    <Footer setSelectedPage={setSelectedPage}/>
+    <Footer/>
   </div>
 }
 
